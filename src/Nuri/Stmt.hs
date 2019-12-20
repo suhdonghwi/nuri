96d7066e43f5e6e3d213984aa98511ddc6d1c9dd
suhdonghwi <hwidongsuh@gmail.com>
2019-12-20 17:28:31 +0900

    Stmt 타입 생성자 마다 주석 추가

--- src/Nuri/Stmt.hs
+++ src/Nuri/Stmt.hs
@@ -5,13 +5,18 @@ import           Text.Megaparsec.Pos                      ( SourcePos )
 import           Nuri.Expr
 import           Nuri.ASTNode
 
-
-data Stmt = ExprStmt Expr
-          | Return Expr
-          | Assign SourcePos Text Expr
-          | If SourcePos Expr Stmts (Maybe Stmts)
-          | While Expr Stmts
-          | FuncDecl SourcePos Text [Text] Stmts
+data Stmt = -- 표현식 구문 : 표현식
+              ExprStmt Expr
+            -- 반환 구문 : 표현식
+            | Return Expr
+            -- 대입 구문 : 코드 위치, 대입 대상 식별자, 대입하는 식
+            | Assign SourcePos Text Expr
+            -- 조건 분기 구문 : 코드 위치, 조건식, True시 실행 구문 집합, False시 실행 구문 집합
+            | If SourcePos Expr Stmts (Maybe Stmts)
+            -- 조건 반복 구문 : 조건식, 반복할 구문 집합
+            | While Expr Stmts
+            -- 함수 선언 구문 : 코드 위치, 함수 이름, 함수 인자 이름, 함수 내부 구문 집합
+            | FuncDecl SourcePos Text [Text] Stmts
           deriving (Show)
 
 type Stmts = NonEmpty Stmt
