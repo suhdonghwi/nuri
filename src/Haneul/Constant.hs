b694c0d8fdab6f648051b3dfa2d6971df8c00392
suhdonghwi <hwidongsuh@gmail.com>
2020-01-02 21:55:56 +0900

    FuncObject 다시 추가 및 테스트 케이스 코드 수정

--- src/Haneul/Constant.hs
+++ src/Haneul/Constant.hs
@@ -1,8 +1,25 @@
 module Haneul.Constant where
 
+import           Control.Lens                             ( makeLenses )
+import           Data.Set.Ordered                         ( OSet )
+import           Text.Megaparsec.Pos                      ( SourcePos )
+
+import           Haneul.Instruction                       ( Instruction )
+
 data Constant = ConstInteger Integer
               | ConstReal Double
               | ConstChar Char
               | ConstBool Bool
+              | ConstFunc FuncObject
   deriving (Eq, Show, Ord)
+
+
+data FuncObject = FuncObject { _arity :: Integer,
+                               _insts :: [(SourcePos, Instruction)],
+                               _funcConstTable :: OSet Constant,
+                               _funcVarNames :: OSet Text
+                             }
+  deriving (Eq, Show, Ord)
+
+$(makeLenses ''FuncObject)
 
--- src/Nuri/Codegen/Stmt.hs
+++ src/Nuri/Codegen/Stmt.hs
@@ -1,14 +1,20 @@
 module Nuri.Codegen.Stmt where
 
-import           Control.Monad.RWS                        ( tell )
+import           Control.Monad.RWS                        ( tell
+                                                          , execRWS
+                                                          )
+import           Control.Lens                             ( view )
 
 import           Text.Megaparsec.Pos                      ( SourcePos )
+
+import qualified Data.Set.Ordered              as S
 
 import           Nuri.Stmt
 import           Nuri.ASTNode
 import           Nuri.Codegen.Expr
 
 import           Haneul.Builder
+import           Haneul.Constant
 import qualified Haneul.Instruction            as Inst
 
 compileStmt :: Stmt -> Builder ()
@@ -21,9 +27,23 @@ compileStmt stmt@(Return expr) = do
 compileStmt (Assign pos ident expr) = do
   compileExpr expr
   storeVar pos ident
-compileStmt If{}       = undefined
-compileStmt While{}    = undefined
-compileStmt FuncDecl{} = undefined
+compileStmt If{}                                  = undefined
+compileStmt While{}                               = undefined
+compileStmt (FuncDecl pos funcName argNames body) = do
+  let (internal, instructions) = execRWS
+        (sequence_ (compileStmt <$> body))
+        ()
+        (defaultInternal { _varNames = S.fromList argNames })
+      funcObject = ConstFunc
+        (FuncObject { _arity          = fromIntegral (length argNames)
+                    , _insts          = instructions
+                    , _funcConstTable = view constTable internal
+                    , _funcVarNames   = view varNames internal
+                    }
+        )
+  funcObjectIndex <- addConstant funcObject
+  funcNameIndex   <- addVarName funcName
+  tell [(pos, Inst.Push funcObjectIndex), (pos, Inst.Store funcNameIndex)]
 
 storeVar :: SourcePos -> Text -> Builder ()
 storeVar pos ident = do
