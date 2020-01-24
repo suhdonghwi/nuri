f347f2030af0ff91e1a13670f70e54ee9eb205bb
suhdonghwi <hwidongsuh@gmail.com>
2020-01-24 23:36:20 +0900

    BuilderInternal Eq 인스턴스 정의 제거

--- src/Haneul/BuilderInternal.hs
+++ src/Haneul/BuilderInternal.hs
@@ -8,11 +8,7 @@ import           Data.Set.Ordered                         ( OSet )
 import           Haneul.Constant
 
 data BuilderInternal = BuilderInternal { _internalConstTable :: ConstTable, _internalVarNames :: OSet (String, Int), _internalOffset :: Int32, _internalMarks :: [Int32] }
-  deriving (Show)
-
-instance Eq BuilderInternal where
-  BuilderInternal t1 v1 _ m1 == BuilderInternal t2 v2 _ m2 =
-    (t1 == t2) && (v1 == v2) && (m1 == m2)
+  deriving (Eq, Show)
 
 $(makeLenses ''BuilderInternal)
 
