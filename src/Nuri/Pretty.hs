893e885d5dd5dad546b77eacf261cabe27e4f8b7
suhdonghwi <hwidongsuh@gmail.com>
2020-02-01 19:12:40 +0900

    Expr Pretty 정의 빠진 부분 추가

--- src/Nuri/Pretty.hs
+++ src/Nuri/Pretty.hs
@@ -48,6 +48,12 @@ instance Pretty Expr where
   pretty (Var _ ident         ) = "Var" <+> parens ((dquotes . pretty) ident)
   pretty (FuncCall _ func args) = nest'
     $ vsep ["App", "[func]" <+> pretty func, "[args]" <+> align (pretty args)]
+  pretty (If _ cond thenExpr elseExpr) = nest' $ vsep
+    [ "If"
+    , "[cond]" <+> pretty cond
+    , "[then]" <+> pretty thenExpr
+    , "[else]" <+> pretty elseExpr
+    ]
   pretty (BinaryOp _ op lhs rhs) = nest' $ vsep
     [ "BinaryOp"
     , "[op]" <+> pretty op
@@ -57,6 +63,15 @@ instance Pretty Expr where
   pretty (UnaryOp _ op val) =
     nest' $ vsep ["UnaryOp", "[op]" <+> pretty op, "[val]" <+> pretty val]
   pretty (List _ list) = nest' $ vsep ["List", "[elements]" <+> pretty list]
+  pretty (Seq exprs  ) = nest' $ vsep ["Seq", pretty exprs]
+  pretty (Lambda _ args body) =
+    nest' $ vsep ["Lambda", "[args]" <+> pretty args, "[body]" <+> pretty body]
+  pretty (Let _ name value expr) = nest' $ vsep
+    [ "Let"
+    , "[name]" <+> pretty name
+    , "[value]" <+> pretty value
+    , "[expr]" <+> pretty expr
+    ]
 
 instance Pretty Stmt where
   pretty (DeclStmt decl) = nest' $ vsep ["DeclStmt", pretty decl]
