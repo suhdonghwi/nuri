bced91793ac8779903da975b7019d0beb4a4a9fa
suhdonghwi <hwidongsuh@gmail.com>
2020-02-19 22:27:46 +0900

    Let 표현식 코드 생성 테스트 케이스 추가

--- test/Nuri/Spec/Codegen/ExprSpec.hs
+++ test/Nuri/Spec/Codegen/ExprSpec.hs
@@ -161,33 +161,42 @@ spec = do
                         ]
                       , [Inst.Push 0]
                       )
-  -- describe "상수 선언문 코드 생성" $ do
-  --   it "하나의 값에 대한 상수 선언문 코드 생성" $ do
-  --     compileExpr
-  --         (letExpr "값" (litInteger 1) (binaryOp Add (var "값") (litInteger 2)))
-  --       `shouldBuild` ( S.fromList [ConstInteger 1, ConstInteger 2]
-  --                     , [ Inst.Push 0
-  --                       , Inst.Store "값"
-  --                       , loadGlobal "값"
-  --                       , Inst.Push 1
-  --                       , Inst.Add
-  --                       ]
-  --                     )
-  --   it "계산식 상수 선언문 코드 생성" $ do
-  --     compileExpr
-  --         (letExpr "값"
-  --                  (binaryOp Add (litInteger 1) (litInteger 2))
-  --                  (binaryOp Add (var "값") (litInteger 3))
-  --         )
-  --       `shouldBuild` ( S.fromList
-  --                       [ConstInteger 1, ConstInteger 2, ConstInteger 3]
-  --                     , [ Inst.Push 0
-  --                       , Inst.Push 1
-  --                       , Inst.Add
-  --                       , Inst.Store "값"
-  --                       , loadGlobal "값"
-  --                       , Inst.Push 2
-  --                       , Inst.Add
-  --                       ]
-  --                     )
+  describe "상수 선언문 코드 생성" $ do
+    it "하나의 값에 대한 상수 선언문 코드 생성" $ do
+      compileExpr
+          (letExpr "값" (litInteger 1) (binaryOp Add (var "값") (litInteger 2)))
+        `shouldBuild` ( S.fromList
+                        [ ConstInteger 1
+                        , ConstFunc
+                          (FuncObject
+                            1
+                            (ann [Inst.Load 0, Inst.Push 0, Inst.Add])
+                            (S.singleton (ConstInteger 2))
+                          )
+                        ]
+                      , [Inst.Push 0, Inst.Push 1, Inst.Call 1]
+                      )
+    it "계산식 상수 선언문 코드 생성" $ do
+      compileExpr
+          (letExpr "값"
+                   (binaryOp Add (litInteger 1) (litInteger 2))
+                   (binaryOp Add (var "값") (litInteger 3))
+          )
+        `shouldBuild` ( S.fromList
+                        [ ConstInteger 1
+                        , ConstInteger 2
+                        , ConstFunc
+                          (FuncObject
+                            1
+                            (ann [Inst.Load 0, Inst.Push 0, Inst.Add])
+                            (S.singleton (ConstInteger 3))
+                          )
+                        ]
+                      , [ Inst.Push 0
+                        , Inst.Push 1
+                        , Inst.Add
+                        , Inst.Push 2
+                        , Inst.Call 1
+                        ]
+                      )
 
