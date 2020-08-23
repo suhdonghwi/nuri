dd5d09b7987d749217f5e7d16bebdd7fdcceb593
suhdonghwi <hwidongsuh@gmail.com>
2020-08-23 20:33:59 +0900

    docopt 라이브러리 추가

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,41 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import           System.Environment
+import Control.Monad (when)
+import Data.Maybe (fromJust)
+import Repl
+import System.Console.Docopt
+import System.Environment (getArgs)
 
-import           Repl
+patterns :: Docopt
+patterns = [docoptFile|USAGE.txt|]
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    []           -> runRepl repl (ReplState ">> ")
-    filePath : _ -> do
-      content <- readFileText filePath
-      result  <- runMaybeT $ parseInput content filePath
-      whenJust result printResult
+  opts <- parseArgsOrExit patterns =<< getArgs
+
+  when (opts `isPresent` (longOption "help")) $ do
+    let helpMessage =
+          [ "누리 - 함수형 한글 프로그래밍 언어",
+            "",
+            "사용법:",
+            "  nuri --help | -h",
+            "  nuri --version | -v",
+            "  nuri <파일명>",
+            "",
+            "옵션:",
+            "  -h, --help      도움말을 출력합니다.",
+            "  -v, --version   누리 실행기의 버전을 출력합니다."
+          ] ::
+            [Text]
+    putTextLn $ unlines helpMessage
+    exitSuccess
+  when (opts `isPresent` (longOption "version")) $ do
+    putStrLn "누리 - 배포 전 0.1"
+    exitSuccess
+  when (opts `isPresent` (argument "file")) $ do
+    let filePath = fromJust $ opts `getArg` (argument "file")
+    content <- readFileText filePath
+    result <- runMaybeT $ parseInput content filePath
+    whenJust result printResult
